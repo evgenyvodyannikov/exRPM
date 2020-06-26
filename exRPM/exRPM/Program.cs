0856f987dd1a67c0ddb0502f43e6bfd9cbcb8ef0
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-26 13:16:54 +0500

    диалоговая форма  + модификаторы доступа

--- exRPM/exRPM/Program.cs
+++ exRPM/exRPM/Program.cs
@@ -16,7 +16,7 @@ namespace exRPM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
